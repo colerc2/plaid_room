59a8f25115d40706ff6945609d9ce1278a3c69be
Bob Cole <cole.robert.c@gmail.com>
2014-10-13 01:34:19 -0400

    caught a few errors, still testing though

--- src/discogs_interface.py
+++ src/discogs_interface.py
@@ -4,16 +4,17 @@ import discogs_client
 import urllib2
 import time
 import csv
+import re
 
 class DiscogsClient():
     def __init__(self):
-        self.rate_limit = 1
+        self.rate_limit = 2
         self.connected = False
         #try to connect
         try:
             self.client = discogs_client.Client('Plaid Room Interface')
             self.connected = True
-        except e:
+        except Exception as e:
             #TODO: be more specific about errors and maybe display a pop up window here?
             print 'Problem establishing connection with discogs interface: %s' % e
 
@@ -37,10 +38,22 @@ class DiscogsClient():
         if(self.reconnect_if_necessary()):
             try:
                 results = self.client.search(upc, type='release')
-            except e:
+            except Exception as e:
                 print 'Failed on the call to discogs client: %s' % e
         else:
             #reconnect failed, throw some error breh
             print 'Some shit is going down, figure out what'
         return results
+
+    def clean_up_upc(self, upc):
+        new_upc = re.sub(r"\D", "", upc)
+        new_upc = new_upc.strip()
+        return new_upc
+        
+    def does_this_even_make_sense(self, upc):
+        #first get rid of anything that isn't a number
+        if (len(upc) != 12 and len(upc) != 13):
+            return False
+        else:
+            return True
 
--- src/discogs_tester.py
+++ src/discogs_tester.py
@@ -3,10 +3,63 @@
 import discogs_client
 import time
 from discogs_interface import DiscogsClient
+import csv
 
 
 if __name__ == "__main__":
+    #create discogs object
     discogs = DiscogsClient()
-    result = discogs.search_for_release('659457206512')
     
-    print result[0].title
+    #grab all fat beats catalog
+    f = open('txt_files/fat_beats_catalog.txt')
+    upcs = f.readlines()
+    f.close()
+
+    number_of_problems = 0
+    number_found = 0
+    #grab info from discogs
+    for upc in upcs:
+        upc_checked = False
+        while(upc_checked == False):
+            try:
+                #make sure not to exceed discogs API limit
+                time.sleep(discogs.rate_limit)
+                print '*'*60
+
+                #few quick checks on UPC, then search for it
+                upc = discogs.clean_up_upc(upc)
+                if(not discogs.does_this_even_make_sense(upc)):
+                    #            print '*'*60
+                    print 'This UPC (%s) doesn\'t even make sense.' % upc
+                    upc_checked = True
+                    continue
+                results = discogs.search_for_release(upc)
+
+                #make sure result is "valid"
+                if results is None or len(results) == 0:
+                    #           print '*'*60
+                    print 'No match found on discogs for UPC %s.' % upc
+                    upc_checked = True
+                    continue
+            
+                #       print '*'*60
+                print '%s results found for UPC %s' % (len(results), upc)
+                for result in results:
+                    print '-'*50
+                    print 'ID: \t\t %s' % result.id
+                    print 'Title:\t\t %s' % result.title
+                    print 'Year:\t\t %s' % result.year
+                    for artist in result.artists:
+                        print 'Artist(s):\t\t%s' % artist.name
+                        
+                number_found = number_found + 1
+                upc_checked = True
+
+            except Exception as e:
+                number_of_problems = number_of_problems + 1
+                print 'Something bad happened, retrying for UPC %s.' % upc
+
+    print '*'*50
+    print '*'*50
+    print '%s found out of %s' % (len(number_found), len(upcs))
+    print 'Number of times disconnected: %s' % number_of_problems
