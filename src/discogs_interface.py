e6516cdaf8e83099d37e12ba5d50398310c30019
Bob Cole <cole.robert.c@gmail.com>
2014-10-12 23:01:58 -0400

    not sure what i'm commiting

--- src/discogs_interface.py
+++ src/discogs_interface.py
@@ -7,21 +7,33 @@ import csv
 
 class DiscogsClient():
     def __init__(self):
+        self.rate_limit = 1
         self.connected = False
         #try to connect
         try:
             self.client = discogs_client.Client('Plaid Room Interface')
             self.connected = True
             break
-        except:
+        except e:
             #TODO: be more specific about errors and maybe display a pop up window here?
-            print 'Problem establishing connection with discogs interface'
+            print 'Problem establishing connection with discogs interface: %s' % e
     
     def search_for_release(self, upc):
-        reconnect_if_necessary()
+        if(reconnect_if_necessary()):
+            
+        else:
+            #reconnect failed, throw some error breh
 
     def reconnect_if_necessary(self):
-        if(self.connected):
-            return
-        else:
-            
+        if(!self.connected):
+            #first, make sure we aren't exceeding discogs API's rate limit
+            time.sleep(self.rate_limit)
+            try:
+                self.client = discogs_client.Client('Plaid Room Interface')
+                self.connected = True
+                break
+            except e:
+                #TODO: be more specific about errors and maybe display a pop up window here?
+                print 'Problem establishing connection with discogs interface: %s' % e
+                self.connected = False
+        return self.connected
