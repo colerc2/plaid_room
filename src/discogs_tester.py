aa53260fa031780ae82cb819a9069e082afb1a8e
Bob Cole <cole.robert.c@gmail.com>
2014-10-13 13:49:10 -0400

    testing extraction of info from discogs objects

--- src/discogs_tester.py
+++ src/discogs_tester.py
@@ -29,7 +29,6 @@ if __name__ == "__main__":
                 #few quick checks on UPC, then search for it
                 upc = discogs.clean_up_upc(upc)
                 if(not discogs.does_this_even_make_sense(upc)):
-                    #            print '*'*60
                     print 'This UPC (%s) doesn\'t even make sense.' % upc
                     upc_checked = True
                     continue
@@ -37,28 +36,42 @@ if __name__ == "__main__":
 
                 #make sure result is "valid"
                 if results is None or len(results) == 0:
-                    #           print '*'*60
                     print 'No match found on discogs for UPC %s.' % upc
                     upc_checked = True
                     continue
             
-                #       print '*'*60
+                #display results in terminal
                 print '%s results found for UPC %s' % (len(results), upc)
                 for result in results:
                     print '-'*50
                     print 'ID: \t\t %s' % result.id
                     print 'Title:\t\t %s' % result.title
                     print 'Year:\t\t %s' % result.year
-                    print 'Genres:\t\t %s' % (",".join(result.genres))
+                    print 'Genres:\t\t %s' % (", ".join(result.genres))
+                    prices = [None] * 3
+                    discogs.scrape_price(result.id, prices)
+                    if prices[0] != None:
+                        print 'Price: %s, %s, %s' % (prices[0], prices[1], prices[2])
                     for artist in result.artists:
-                        print 'Artist(s):\t\t%s' % artist.name
+                        print 'Artist:\t\t %s' % artist.name
+                        print 'Real Name:\t%s' % artist.real_name
+                        print 'Profile:\t%s' % artist.profile
+                        print 'Variations:\t%s' % (", ".join(artist.name_variations))
+                        print 'Aliases:\t%s' % (", ".join(artist.aliases))
+                    print 'Track List:'
+                    for t in result.tracklist:
+                        print '\t Track %s - %s - %s' % (t.position, t.duration, t.title)
+
+                    print 'Notes:\t\t %s' % (result.notes)
+                
                         
                 number_found = number_found + 1
                 upc_checked = True
 
             except Exception as e:
                 number_of_problems = number_of_problems + 1
-                print 'Something bad happened, retrying for UPC %s.' % upc
+                print 'Something bad happened, retrying for UPC %s: %s' % (upc, e)
+                time.sleep(2)
 
     print '*'*50
     print '*'*50
