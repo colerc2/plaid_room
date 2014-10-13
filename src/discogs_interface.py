28fe749c7ec0223f58c8d8183ddd77d41edfd74c
Bob Cole <cole.robert.c@gmail.com>
2014-10-12 23:35:25 -0400

    first "test" kind of working, now to more complicated stuff

--- src/discogs_interface.py
+++ src/discogs_interface.py
@@ -13,13 +13,28 @@ class DiscogsClient():
         try:
             self.client = discogs_client.Client('Plaid Room Interface')
             self.connected = True
-            break
         except e:
             #TODO: be more specific about errors and maybe display a pop up window here?
             print 'Problem establishing connection with discogs interface: %s' % e
-    
+
+    def reconnect_if_necessary(self):
+        if(not self.connected):
+            #first, make sure we aren't exceeding discogs API's rate limit
+            time.sleep(self.rate_limit)
+            print 'Attempting to reconnect to discogs API'
+            try:
+                self.client = discogs_client.Client('Plaid Room Interface')
+                self.connected = True
+                print 'Attempted connection was successful'
+            except e:
+                #TODO: be more specific about errors and maybe display a pop up window here?
+                print 'Problem establishing connection with discogs interface: %s' % e
+                self.connected = False
+        return self.connected
+
+ 
     def search_for_release(self, upc):
-        if(reconnect_if_necessary()):
+        if(self.reconnect_if_necessary()):
             try:
                 results = self.client.search(upc, type='release')
             except e:
@@ -29,18 +44,3 @@ class DiscogsClient():
             print 'Some shit is going down, figure out what'
         return results
 
-    def reconnect_if_necessary(self):
-        if(!self.connected):
-            #first, make sure we aren't exceeding discogs API's rate limit
-            time.sleep(self.rate_limit)
-            print 'Attempting to reconnect to discogs API'
-            try:
-                self.client = discogs_client.Client('Plaid Room Interface')
-                self.connected = True
-                print 'Attempted connection was successful'
-                break
-            except e:
-                #TODO: be more specific about errors and maybe display a pop up window here?
-                print 'Problem establishing connection with discogs interface: %s' % e
-                self.connected = False
-        return self.connected
--- src/discogs_tester.py
+++ src/discogs_tester.py
@@ -2,10 +2,11 @@
 
 import discogs_client
 import time
+from discogs_interface import DiscogsClient
 
 
 if __name__ == "__main__":
     discogs = DiscogsClient()
     result = discogs.search_for_release('659457206512')
     
-    print result.title
+    print result[0].title
